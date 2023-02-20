c2297e931f896b46cbd8b23d1dd5656a90eacdfd
alvin tan <alvintan95@gmail.com>
2023-02-20 16:19:26 +0800

    Learning the basics of kotlin...

--- src/main/kotlin/com/aattwwss/springbootauthfiltersdemo/SpringBootAuthFiltersDemoApplication.kt
+++ src/main/kotlin/com/aattwwss/springbootauthfiltersdemo/SpringBootAuthFiltersDemoApplication.kt
@@ -1,9 +1,10 @@
 package com.aattwwss.springbootauthfiltersdemo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [SecurityAutoConfiguration::class])
 class SpringBootAuthFiltersDemoApplication
 
 fun main(args: Array<String>) {
