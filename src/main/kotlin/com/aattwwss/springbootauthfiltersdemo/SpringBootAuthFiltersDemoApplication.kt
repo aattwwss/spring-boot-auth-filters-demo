8e32fe9458c249dc8d39c648ad179c52630b8a62
alvin tan <alvintan95@gmail.com>
2023-02-20 15:06:40 +0800

    init

---
+++ src/main/kotlin/com/aattwwss/springbootauthfiltersdemo/SpringBootAuthFiltersDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.aattwwss.springbootauthfiltersdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootAuthFiltersDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBootAuthFiltersDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/aattwwss/springbootauthfiltersdemo/SpringBootAuthFiltersDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.aattwwss.springbootauthfiltersdemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootAuthFiltersDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
